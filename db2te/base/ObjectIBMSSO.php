433946422d4c27ae01d7bf5ac0e54956f9dc1dd8
Peter Prib <peter.prib@pribonline.com>
2014-05-17 22:03:44 +1000

    fix

--- db2te/base/ObjectIBMSSO.php
+++ db2te/base/ObjectIBMSSO.php
@@ -124,14 +124,16 @@ class IBMSSO {
 		if ($curl_errno > 0)
 			throw new Exception($curl_error);
 		if($RawData==null ||  $RawData=="")
-			throw new Exception('Not data.');
+			throw new Exception('No data returned');
 		if(preg_match('/^Not Found/', $RawData) > 0)
 			throw new Exception('Not found.');
 		$response=json_decode($RawData,true);
-		if(array_key_exists('error',$response))
-			throw new Exception($response['error_description']);
+		if($response!=null)
+			if(is_array($response))
+				if(array_key_exists('error',$response))
+					throw new Exception($response['error_description']);
 		error_log("trace reponse: ".var_export($response,true),0);
-		
+
 	}
 	function getState($state) {
 		return $this->state;
