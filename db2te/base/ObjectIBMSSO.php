e19d8d6b3dbd353fc9f85c56fb28c475245a359c
Peter Prib <peter.prib@pribonline.com>
2014-05-18 19:14:07 +1000

    fix sso

--- db2te/base/ObjectIBMSSO.php
+++ db2te/base/ObjectIBMSSO.php
@@ -89,7 +89,8 @@ class IBMSSO {
  */
 	}
 	function getBearerAccessToken() {
-		return $this->getBearerToken()['access_token'];
+		$token=$this->getBearerToken();
+		return $token['access_token'];
 	}
 	function getBearerToken() {
 		if($this->tokenBearer!=null)
